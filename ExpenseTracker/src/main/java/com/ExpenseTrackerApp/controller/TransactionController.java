9f6b59ed72fbfd8a6ce6b20ae291595e59563c4d
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-16 04:09:13 +0100

    third commit

--- ExpenseTracker/src/main/java/com/ExpenseTrackerApp/controller/TransactionController.java
+++ ExpenseTracker/src/main/java/com/ExpenseTrackerApp/controller/TransactionController.java
@@ -1,11 +1,33 @@
 package com.ExpenseTrackerApp.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ExpenseTrackerApp.model.Transaction;
+import com.ExpenseTrackerApp.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
 @CrossOrigin(origins = "*")
 public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<String> addTransaction(@RequestBody Transaction transaction){
+        transactionService.addTransaction(transaction);
+        return ResponseEntity.ok("Transaction added successfully");
+    }
+    @GetMapping("/{username}")
+    public ResponseEntity<List<Transaction>> getUserTransactions(@PathVariable String username){
+        return ResponseEntity.ok(transactionService.getTransactionByUser(username))
+    }
+    @DeleteMapping("/{username}/{id}")
+    public ResponseEntity<String> deleteTransaction(@PathVariable String username, @PathVariable String id){
+        transactionService.deleteTransaction(username, id);
+        return ResponseEntity.ok("Transaction deleted successfully");
+    }
 }
--- ExpenseTracker/src/main/java/com/ExpenseTrackerApp/service/TransactionService.java
+++ ExpenseTracker/src/main/java/com/ExpenseTrackerApp/service/TransactionService.java
@@ -1,4 +1,30 @@
 package com.ExpenseTrackerApp.service;
 
+import com.ExpenseTrackerApp.model.Transaction;
+import com.ExpenseTrackerApp.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public void addTransaction(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getTransactionByUsername(String username){
+        return transactionRepository.findByUsername(username);
+    }
+    public void deleteTransaction(String username, String id){
+        Transaction transaction = transactionRepository.findById(id).filter(t -> t.getUsername().equals(username)).orElse(null);
+
+        if(transaction != null){
+            transactionRepository.deleteById(id);
+        }
+    }
 }
